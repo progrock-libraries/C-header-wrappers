9204f6a6ae2d839186a756a5f89b6cfa05119dca
Alf P. Steinbach <alf.p.steinbach@gmail.com>
2018-11-28 11:24:53 +0100

    -

--- source/c/assert.hpp
+++ source/c/assert.hpp
@@ -4,4 +4,5 @@
 // The `assert` macro. Note: can be reincluded with/without NDEBUG defined.
 // Copyright © 2017 Alf P. Steinbach, distributed under Boost license 1.0.
 
+#include <cassert>          // No particular reason to, but follows the general scheme.
 #include <assert.h>
