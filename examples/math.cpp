e0cab880c25893ddbf0520609fb76a4e8fc7f470
Alf P. Steinbach <alf.p.steinbach@gmail.com>
2018-11-28 10:21:29 +0100

    Initial commit

---
+++ examples/math.cpp
@@ -0,0 +1,19 @@
+﻿#include <c/float.hpp>          // DBL_DIG
+#include <c/math.hpp>           // abs, M_PI
+#include <iostream>
+#include <iomanip>              // std::setprecision    
+
+using C_str = const char*;
+
+auto type_name( int ) -> C_str      { return "int"; }
+auto type_name( double ) -> C_str   { return "double"; }
+
+auto main() -> int
+{
+    using std::cout, std::endl, std::setprecision;
+    
+    cout << setprecision( DBL_DIG )
+        << "`M_PI` is a " << type_name( M_PI ) << " of value ~= " << abs( -M_PI )
+        << ", and `abs(42)` is an " << type_name( abs( 42 ) ) << "."
+        << endl;
+}
---
+++ source/c/signal.hpp
@@ -0,0 +1,12 @@
+﻿#pragma once    // Source encoding: UTF-8 (π is a lowercase Greek "pi").
+// #include <c/signal.hpp>
+//
+// Functions and macro constants for signal management.
+// Copyright © 2017 Alf P. Steinbach, distributed under Boost license 1.0.
+
+#ifndef REALLY_WANT_SIGNAL
+#   error `signal` doesn't work well with C++. You can define REALLY_WANT_SIGNAL.
+#endif
+
+#include <csignal>
+#include <signal.h>
